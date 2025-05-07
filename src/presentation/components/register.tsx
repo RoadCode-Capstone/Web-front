d4669492a053a7a91f9ecfc5373893ff4d70a2e5
ParkTjgus <129525514+ParkTjgus@users.noreply.github.com>
2025-05-07 16:14:37 +0900

    feat: "회원가입 caption 추가"
    회원가입 시 각 input 값에 대한 결과 caption 추가. 현재는 입력값에 따라 변경되지는 않고 default 값이 정적으로 보여짐. 수정해야할 사항 TODO로 작성해둠

--- src/presentation/components/register.tsx
+++ src/presentation/components/register.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import styles from "../styles/register-style.module.css";
 
 export const Register: React.FC = () => {
+
+  //TODO
+  //1. 이메일 값에 따른 caption 수정: 사용할 수 있는 ID입니다 || 이미 가입된 이메일입니다
+  //2. 비밀번호 값에 따른 caption 수정: 사용할 수 있는 비밀번호입니다 || 특수문자가 포함되어야 합니다
+  //3. 비밀번호 한번 더 입력 했을 때 틀렸을 경우에만 caption 출력
+  //4. 중복되는 닉네임 입력 시에만 caption 출력
+  
   return (
     <div className={styles.container}>
       <form className={styles.form}>
@@ -12,25 +19,26 @@ export const Register: React.FC = () => {
             className={styles.input}
             placeholder="이메일을 입력하세요"
           />
+          <span className={styles.caption_left}>사용할 수 있는 ID입니다</span>
           <input
             type="password"
             className={styles.input}
             placeholder="비밀번호를 입력하세요"
           />
+          <span className={styles.caption_left}>사용할 수 있는 비밀번호입니다</span>
           <input
             type="password"
             className={styles.input}
             placeholder="비밀번호를 한 번 더 입력하세요"
           />
+          <span className={styles.caption_left}>일치하지 않는 비밀번호입니다</span>
           <input
             type="text"
             className={styles.input}
             placeholder="사용할 닉네임을 입력하세요"
           />
+          <span className={styles.caption_left}>중복되는 닉네임입니다</span>
         </div>
-        <a href="#" className={`${styles.caption} ${styles.forgetPassword}`}>
-          비밀번호를 잊어버리셨습니까?
-        </a>
         <input
           type="submit"
           className={styles.submit}
