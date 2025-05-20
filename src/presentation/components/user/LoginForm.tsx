437bb6f514fbfdad264cfc0ca77fd915b8fd7e37
ParkTjgus <129525514+ParkTjgus@users.noreply.github.com>
2025-05-20 17:19:56 +0900

    feat: LoginForm 구현

---
+++ src/presentation/components/user/LoginForm.tsx
@@ -0,0 +1,32 @@
+import Button from "../common/Button";
+import InputField from "../common/InputField";
+
+const LoginForm = () => {
+  return (
+    <form action="#" method="GET" className="w-[456px]">
+      <div className="flex flex-col gap-y-6">
+        <InputField
+          iconProps={{ name: "user", size: 24 }}
+          placeholder="이메일을 입력하세요"
+          type="text"
+          containerStyle="w-[456px]"
+        />
+        <InputField
+          iconProps={{ name: "lock", size: 24 }}
+          placeholder="비밀번호를 입력하세요"
+          type="password"
+          containerStyle="w-[456px]"
+        />
+      </div>
+      <div className="flex justify-end my-2">
+        <a href="/passwordFinding" className="text-sm">
+          비밀번호를 잊어버리셨습니까?
+        </a>
+      </div>
+
+      <Button label="로그인" buttonStyle="w-[456px] my-6" />
+    </form>
+  );
+};
+
+export default LoginForm;
