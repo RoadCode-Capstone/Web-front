63fff4efca7b373bbb45078a36c05f2ee6b836c8
ParkTjgus <psh22112100@gmail.com>
2025-06-20 13:27:53 +0900

    chore: ApiResponse 정의 및 ApiHeader 설정

---
+++ src/constants/api.ts
@@ -0,0 +1,4 @@
+export const API_PREFIX = `${import.meta.env.VITE_API_SERVER_URL}/api/v1`;
+export const API_TEST_TOKEN = `${import.meta.env.VITE_API_TEST_TOKEN_TYPE} ${
+  import.meta.env.VITE_API_TEST_TOKEN_VALUE
+}`;
