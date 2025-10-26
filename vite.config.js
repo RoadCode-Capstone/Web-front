72deb4000548a7e27154614e9c44bc20b1dbf8ef
ParkTjgus <psh22112100@gmail.com>
2025-10-26 20:19:21 +0900

    chore: installation vite-plugin-svgr

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig(() => {
   return {
     build: {
       outDir: "build",
     },
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), svgr()],
   };
 });
