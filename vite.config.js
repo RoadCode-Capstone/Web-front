73d158334fda205740cbe53fe0a9890c951e5b22
ParkTjgus <psh22112100@gmail.com>
2025-11-08 19:56:19 +0900

    chore: path alias 설정
    - src: @
    - assets: @assets

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import path from "path";
 
 export default defineConfig(() => {
   return {
@@ -9,5 +10,14 @@ export default defineConfig(() => {
       outDir: "build",
     },
     plugins: [react(), tailwindcss(), svgr()],
+    resolve: {
+      alias: [
+        { find: "@", replacement: path.resolve(__dirname, "src") },
+        {
+          find: "@assets",
+          replacement: path.resolve(__dirname, "src/presentation/assets"),
+        },
+      ],
+    },
   };
 });
