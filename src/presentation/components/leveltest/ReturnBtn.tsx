f9e1c911c75840dffb7a959f411350a4ed6c9987
ParkTjgus <psh22112100@gmail.com>
2025-11-08 21:12:00 +0900

    chore: return 및 skip 버튼 navigation 처리
    - return btn 클릭 시 메인페이지로 이동
    - skip btn 클릭 시 /leveltest로 이동

--- src/presentation/components/leveltest/ReturnBtn.tsx
+++ src/presentation/components/leveltest/ReturnBtn.tsx
@@ -1,14 +1,15 @@
+import { useNavigate } from "react-router-dom";
 import Button from "../common/Button";
 import IconBack from "@assets/icons/chevron_backward.svg?react";
 
 export default function ReturnBtn() {
+  const navigate = useNavigate();
+
   return (
     <Button
       leftIcon={<IconBack />}
       text={"돌아가기"}
-      onClick={function (): void {
-        throw new Error("Function not implemented.");
-      }}
+      onClick={() => navigate("/")}
       colorTheme={"point-primary"}
       style={
         "bg-transparent hover:bg-gray-50 hover:text-point text-point gap-x-1 w-[112px] h-[56px]"
