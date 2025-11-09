aaeac7c2d53ea35b30b21527bf22cda30482b478
ParkTjgus <psh22112100@gmail.com>
2025-11-10 00:05:59 +0900

    feat: CodeEditor 모달 구현

--- src/presentation/components/common/CartoonButton.tsx
+++ src/presentation/components/common/CartoonButton.tsx
@@ -10,6 +10,7 @@ interface BubbleBtnProps {
   position?: Position;
   className?: string;
   onClick?: () => void;
+  style?: React.CSSProperties;
 }
 
 // 2. 컴포넌트 구현
@@ -18,6 +19,7 @@ export default function BubbleBtn({
   position = "bottom",
   className,
   onClick,
+  style,
 }: BubbleBtnProps) {
   // 3. 기본 스타일과 위치별 스타일 정의 (em 단위 유지)
   const baseStyle =
@@ -27,9 +29,9 @@ export default function BubbleBtn({
     top: "before:absolute before:content-[''] before:left-[4em] before:bottom-full before:border-x-[2em] before:border-b-[2em] before:border-x-transparent before:border-b-[#B9DDFF]",
     bottom:
       "before:absolute before:content-[''] before:left-[4em] before:top-full before:border-r-[32px] before:border-t-[32px] before:border-r-transparent before:border-t-[#B9DDFF]",
-    left: "before:absolute before:content-[''] before:top-1/2 before:-translate-y-1/2 before:right-full before:border-y-[2em] before:border-r-[2em] before:border-y-transparent before:border-r-[#B9DDFF]",
+    left: "before:absolute before:content-[''] before:left-[var(--tail-offset,4rem)] before:top-full before:border-r-[32px] before:border-t-[32px] before:border-r-transparent before:border-t-[var(--tail-color,#b9ddff)]",
     right:
-      "before:absolute before:content-[''] before:top-1/2 before:-translate-y-1/2 before:left-full before:border-y-[2em] before:border-l-[2em] before:border-y-transparent before:border-l-[#B9DDFF]",
+      "before:absolute before:content-[''] before:right-[var(--tail-offset,4rem)] before:top-full before:border-l-[32px] before:border-t-[32px] before:border-l-transparent before:border-t-[var(--tail-color,#b9ddff)]",
   };
 
   // 4. 클래스 조합
@@ -41,6 +43,7 @@ export default function BubbleBtn({
   return (
     <button
       onClick={onClick}
+      style={style}
       className={cn(
         buttonClassName,
         "drop-shadow-[10px_20px_0px_rgba(0,0,0,0.25)]",
