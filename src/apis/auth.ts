1061ba9d30a7abc729b2ad97d32eba030a0b6e89
ParkTjgus <psh22112100@gmail.com>
2025-11-11 20:49:33 +0900

    chore: signup과 login api 를 fetch로 수정

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { API_PREFIX, API_TEST_TOKEN } from "../constants/api";
+import { API_HEADER, API_PREFIX, API_TEST_TOKEN } from "../constants/api";
 import { ApiResponse } from "../types/api";
 import { Auth, AuthLoginRequest, AuthLoginResponse } from "../types/auth";
 
@@ -7,8 +7,13 @@ const AUTH_PREFIX = `${API_PREFIX}/auth`;
 
 export const signup = async (request: Auth) => {
   try {
-    const axiosResponse = await axios.post(`${AUTH_PREFIX}/signup`, request);
-    const response: ApiResponse<null> = axiosResponse.data;
+    const rawResponse = await fetch(`${AUTH_PREFIX}/signup`, {
+      method: "Post",
+      body: JSON.stringify(request),
+      headers: API_HEADER,
+    });
+
+    const response: ApiResponse<null> = await rawResponse.json();
     if (response.code != "SUCCESS") {
       throw new Error(response.message);
     }
@@ -23,14 +28,20 @@ export const login = async (
   request: AuthLoginRequest
 ): Promise<AuthLoginResponse> => {
   try {
-    const axiosResponse = await axios.post(`${AUTH_PREFIX}/login`, request);
-    const response: ApiResponse<AuthLoginResponse> = axiosResponse.data;
+    const response = await fetch(`${AUTH_PREFIX}/login`, {
+      method: "Post",
+      body: JSON.stringify(request),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const result: ApiResponse<AuthLoginResponse> = await response.json();
 
-    if (response.code != "SUCCESS" || response.data == null) {
-      throw new Error(response.message);
+    if (result.code != "SUCCESS" || result.data == null) {
+      throw new Error(result.message);
     }
 
-    return response.data;
+    return result.data;
   } catch (error) {
     throw error;
   }
@@ -38,13 +49,13 @@ export const login = async (
 
 export const logout = async () => {
   try {
-    const axiosResponse = await axios.post(`${AUTH_PREFIX}/logout`, {
+    const rawResponse = await axios.post(`${AUTH_PREFIX}/logout`, {
       headers: {
         Authorization: API_TEST_TOKEN,
       },
     });
 
-    const response: ApiResponse<string> = axiosResponse.data;
+    const response: ApiResponse<string> = rawResponse.data;
 
     if (response.code != "SUCCESS") {
       throw new Error(response.message);
