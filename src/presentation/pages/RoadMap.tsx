3630a5c780622ac00a93f94b02a087cee85676fb
ParkTjgus <psh22112100@gmail.com>
2025-11-14 18:18:33 +0900

    feat: 이전/이후 문제 호출

--- src/presentation/pages/RoadMap.tsx
+++ src/presentation/pages/RoadMap.tsx
@@ -17,6 +17,7 @@ export function RoadMap() {
   const navigate = useNavigate();
   const { state: roadmapId } = useLocation();
 
+  const [inprogOrder, setInprogOrder] = useState<number>(0);
   const [currentProblemOrder, setCurrentProblemOrder] = useState<number>(0);
   const [problems, setProblems] = useState<RoadmapProblem[]>([]);
   const [currProblem, setCurrProblem] = useState<problemRes>();
@@ -42,6 +43,7 @@ export function RoadMap() {
           // 3. 모든 상태 한 번에 업데이트
           setProblems(allProblems);
           setCurrentProblemOrder(currentOrder);
+          setInprogOrder(currentOrder);
           setCurrProblem(problemResponse);
         } catch (error) {
           console.error("로드맵 정보를 불러오는 데 실패했습니다.", error);
@@ -53,6 +55,26 @@ export function RoadMap() {
     }
   }, [roadmapId, navigate]);
 
+  const handleNext = async () => {
+    if (currentProblemOrder >= problems.length - 1) {
+    } else {
+      setCurrentProblemOrder((currentProblemOrder) => currentProblemOrder + 1);
+      const currentProblemId = problems[currentProblemOrder].problemId;
+      const problemResponse = await getProblem(currentProblemId);
+      setCurrProblem(problemResponse);
+    }
+  };
+
+  const handlePrev = async () => {
+    if (currentProblemOrder <= 0) {
+    } else {
+      setCurrentProblemOrder((currentProblemOrder) => currentProblemOrder - 1);
+      const currentProblemId = problems[currentProblemOrder].problemId;
+      const problemResponse = await getProblem(currentProblemId);
+      setCurrProblem(problemResponse);
+    }
+  };
+
   return (
     <>
       <ProblemInfo
@@ -60,20 +82,22 @@ export function RoadMap() {
         description={currProblem?.description || "오류 발생"}
       />
       <div className="flex items-end justify-center w-full gap-x-[116px] fixed bottom-12 ">
-        <PrevBtn />
+        <PrevBtn onClick={handlePrev} />
         <div className="flex items-center justify-center">
           <IconStar className="z-10 drop-shadow-[0_0_40px_#F2C53D]" />
           <span className="absolute font-bold text-4xl z-10">
-            {currentIndex}
+            {currentProblemOrder + 1}
           </span>
           <div className="fixed bottom-20">
             <Character className="z-0 relative" />
             <div>
               <BubbleBtn
-                text={"학습 로드맵 00% 달성 중"}
+                text={`학습 로드맵 ${
+                  (inprogOrder / problems.length) * 100
+                }% 달성 중`}
                 position="right"
-                style={{ "--tail-offset": "50px" }}
-                className="absolute w-[259px] h-[111px] bottom-[253px] left-[-236px]"
+                style={{ "--tail-offset": "50px", "--tail-color": "#F8E19D" }}
+                className="absolute w-[259px] h-[111px] bottom-[253px] left-[-236px]  bg-point-secondary"
               />
               <BubbleBtn
                 text={"일일 학습 목표 달성까지\n0문제 남음"}
@@ -84,19 +108,19 @@ export function RoadMap() {
               <BubbleBtn
                 text={"문제 추가하기"}
                 position="left"
-                style={{ "--tail-offset": "40px" }}
-                className="absolute w-[192px] h-[82px] bottom-[188px] right-[-179px]"
+                style={{ "--tail-offset": "40px", "--tail-color": "#FBF0CE" }}
+                className="absolute w-[192px] h-[82px] bottom-[188px] right-[-179px] bg-point-teritary hover:bg-point-secondary"
               />
               <BubbleBtn
                 text={"로드맵 포기하기"}
                 position="left"
-                style={{ "--tail-offset": "40px" }}
-                className="absolute w-[192px] h-[82px] bottom-[73px] right-[-234px]"
+                style={{ "--tail-offset": "40px", "--tail-color": "#FBF0CE" }}
+                className="absolute w-[192px] h-[82px] bottom-[73px] right-[-234px] bg-point-teritary hover:bg-point-secondary"
               />
             </div>
           </div>
         </div>
-        <NextBtn />
+        <NextBtn onClick={handleNext} />
       </div>
     </>
   );
