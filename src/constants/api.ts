793c54b3c09a4412064eecf35aa27af2553a3c6f
ParkTjgus <psh22112100@gmail.com>
2025-11-16 22:15:36 +0900

    배포 설정

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,5 @@
 export const API_PREFIX = `${import.meta.env.VITE_API_SERVER}/api/v1`;
+// export const API_PREFIX = "/api";
 export const API_TEST_TOKEN = `${import.meta.env.VITE_API_TEST_TOKEN_TYPE} ${
   import.meta.env.VITE_API_TEST_TOKEN_VALUE
 }`;
